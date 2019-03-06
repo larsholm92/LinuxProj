5aec30ff895d4a95343681c62bb1dae70e352ae8
Lars Holm <larsholm92@gmail.com>
2019-03-06 17:18:19 +0100

    small fix

--- server/startTasklistRpi.sh
+++ server/startTasklistRpi.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# /etc/init.d/startTasklistRpi.sh
+### BEGIN INIT INFO
+# Provides:          startTasklistRpi.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 cd /home/pi/repos/homeServer/LinuxProj/server/routes
 DEBUG=server:* npm start&
 cd /home/pi/repos/homeServer/LinuxProj/server/Client/homepage
